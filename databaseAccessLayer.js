c7f051827f86da01d25a6ff2e6cc48a136a8adaf
sjkang930 <91238446+sjkang930@users.noreply.github.com>
2022-02-22 15:48:39 -0800

    fixed again

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -2,7 +2,7 @@ const database = include('/databaseConnection');
 
 
 function getAllUsers(callback) {
-	let sqlQuery = "SELECT * FROM epyvfbg6yj8cbpkc.web_user;";
+	let sqlQuery = "SELECT web_user_id, first_name, last_name, email FROM web_user";
 	database.query(sqlQuery, (err, results, fields) => {
 		if (err) {
 			callback(err, null);
